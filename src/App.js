0ffe48b3f37d1c8031f2b5df6854c6b50e124c0d
AlfonsopechMag <97648088+AlfonsopechMag@users.noreply.github.com>
2022-01-12 21:42:24 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { FormList } from './components/FormList';
+
+
+
+function App() {
+    
+  return (
+    <div className="container">                  
+        <FormList/>       
+    </div>
+  );
+}
+
+export default App;
